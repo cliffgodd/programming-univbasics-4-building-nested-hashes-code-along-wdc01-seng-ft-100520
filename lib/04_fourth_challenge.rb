0f91193c4d6301a54c1b0e2c3a7f88a0d2d87d5c
Justin Clifford <justoon7.jc@gmail.com>
2020-09-21 01:17:03 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,10 +4,10 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => ["Benvolio" => {
+      :hero_friends => [{
         :name => "Benvolio", :age => "17",
         :attitude => "worried"
-      }, "Mercutio" => {
+      }, {
         :name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"
@@ -19,7 +19,15 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      }, {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
         #your hashes here!
       ]
    }
